568ef6b246a2c6dfadb26b0bf55a0d3f9b7416c9
ecuber <unconfigured@null.spigotmc.org>
2018-07-29 18:43:24 -0400

    Added changes to Updates command

--- src/updates.js
+++ src/updates.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args, cube) => {
-	message.channel.send("**New Updates:** 4x4 and 5x5 are now functional and have proper wide move notation! Also, custom server prefixes are now available! Use !prefix to learn more. The !help command is fancier now! Updates command was also added :wink:");
-	message.channel.send("**Planned Updates:** 4x4 and 5x5 are using an inefficient method, to be updated with RegExp for faster response time. 6x6, 7x7, and some non WCA puzzles will follow in the coming months!");
+	message.channel.send("**New Updates:** All events 2-7 are now fully functional. Thanks to Bacon#1153 for all you've done for the bot! Also, because of a new hosting method, custom server prefixes are fully functional.");
+	message.channel.send("**Planned Updates:** Some non-WCA event scrambles are coming soon.");
 };
 module.exports.config = { name: "updates", aliases: ["whatsnew"] };
