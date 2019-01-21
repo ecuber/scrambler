ea9b34c8b08231d77cd4c44be6cad3b0813b09c9
ecuber <37519236+ecuber@users.noreply.github.com>
2019-01-21 00:50:18 -0500

    Reformatting, forgotten in last commit :-1:
    oopsie doopsie

--- src/stats.js
+++ src/stats.js
@@ -1,8 +1,13 @@
 module.exports.run = async (bot, message, args, cube) => {
-	if(!args[0]) return message.channel.send(`**Humans:**  ${bot.users.filter((usr) => !usr.bot).size}\n**Total Users:**  ${bot.users.size}\n**Total Servers:**  ${bot.guilds.size}`);
-	if(args[0].startsWith("user")) return message.channel.send(`**Humans:**  ${bot.users.filter((usr) => !usr.bot).size}\n**Total:**  ${bot.users.size}`);
-	if(args[0].startsWith("human")) return message.channel.send(`**Humans**:  ${bot.users.filter((usr) => !usr.bot).size}`);
-	if(args[0].startsWith("server") || args[0].startsWith("guild")) return message.channel.send(`**Guilds:**  ${bot.guilds.size}`);
-	return message.channel.send(`**Humans:**  ${bot.users.filter((usr) => !usr.bot).size}\n**Total Users:**  ${bot.users.size}\n**Total Servers:**  ${bot.guilds.size}`);
+	let miliseconds = bot.uptime;
+	let days, hours, minutes, total_hours, total_minutes, total_seconds;
+	total_seconds = parseInt(Math.floor(miliseconds / 1000));
+	total_minutes = parseInt(Math.floor(total_seconds / 60));
+	total_hours = parseInt(Math.floor(total_minutes / 60));
+	days = parseInt(Math.floor(total_hours / 24));
+	minutes = parseInt(total_minutes % 60);
+	hours = parseInt(total_hours % 24);
+	let uptimeStr = `${days} days, ${hours} hours, ${minutes} minutes`;
+	return message.channel.send(`Humans:  ${bot.users.filter((usr) => !usr.bot).size}\nTotal Users:  ${bot.users.size}\nTotal Servers:  ${bot.guilds.size}\nUptime: ${uptimeStr}`);
 };
 module.exports.config = { name: "stats", aliases: ["botstats"] };
