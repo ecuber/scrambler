ce366f7f68f251b56311169e0e8808a3f860b9c1
ecuber <37519236+ecuber@users.noreply.github.com>
2019-03-08 16:11:14 -0500

    MongoDB outage

--- src/updates.js
+++ src/updates.js
@@ -2,9 +2,9 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args, cube) => {
 	let embed = new Discord.RichEmbed()
-		.setTitle("THE COMPETITION UPDATE")
+		.setTitle("Sorry for the inconvenience...")
 		.setColor("RANDOM")
-		.setDescription("This update has everything you need to host competitions on your server!")
+		// .setDescription("This update has everything you need to host competitions on your server!")
 		.addField("Why are things broken?", "Scrambler's server went through an unforseen issue, so all new commands that store data will not work for the time being. Apologies for the inconvenience. Join the support server with `s!invite` for updates on when these features will return.")
 		// .addField("Competition Scrambles", "Long gone are the days of copying and pasting each scramble into the Discord server! Now, configure your competition scrambles easily with \`s!config\` and get all the scrambles at once with \`s!comp\`. These commands are mod-only, and are sure to save you a LOT of time.")
 		// .addField("Competition Results", "Gone too are the days of manual result compilation! Have your members submit times with \`s!submit <event> <average result>\`, and when you're ready do \`s!podiums\` to have the top 3 from every event posted.")
