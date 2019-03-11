88dca749e52e2866674f3f708dbcdaabe2216422
ecuber <37519236+ecuber@users.noreply.github.com>
2019-03-10 22:46:45 -0400

    Revert "Updates on issues"
    This reverts commit 9bdb170b1d6c1b3ab864d4803c2fbe4a53e0d471.

--- src/updates.js
+++ src/updates.js
@@ -5,10 +5,9 @@ module.exports.run = async (bot, message, args, cube) => {
 		.setTitle("THE COMPETITION UPDATE")
 		.setColor("RANDOM")
 		.setDescription("This update has everything you need to host competitions on your server!")
-		.addField("Why are things broken?", "Scrambler's server went through an unforseen issue, so all new commands that store data will not work for the time being. Apologies for the inconvenience. Join the support server with `s!invite` for updates on when these features will return.")
-		// .addField("Competition Scrambles", "Long gone are the days of copying and pasting each scramble into the Discord server! Now, configure your competition scrambles easily with \`s!config\` and get all the scrambles at once with \`s!comp\`. These commands are mod-only, and are sure to save you a LOT of time.")
-		// .addField("Competition Results", "Gone too are the days of manual result compilation! Have your members submit times with \`s!submit <event> <average result>\`, and when you're ready do \`s!podiums\` to have the top 3 from every event posted.")
-		// .addField("MongoDB!", "Scrambler is now completely switched to a proper MongoDB database instead of a flimsy JSON file. Your server's data will be more secure, more organized, and reached more quickly on the server.")
+		.addField("Competition Scrambles", "Long gone are the days of copying and pasting each scramble into the Discord server! Now, configure your competition scrambles easily with \`s!config\` and get all the scrambles at once with \`s!comp\`. These commands are mod-only, and are sure to save you a LOT of time.")
+		.addField("Competition Results", "Gone too are the days of manual result compilation! Have your members submit times with \`s!submit <event> <average result>\`, and when you're ready do \`s!podiums\` to have the top 3 from every event posted.")
+		.addField("MongoDB!", "Scrambler is now completely switched to a proper MongoDB database instead of a flimsy JSON file. Your server's data will be more secure, more organized, and reached more quickly on the server.")
 		.setFooter(`${bot.user.username}`, bot.user.displayAvatarURL)
 		.setTimestamp();
 
