ca364264f28c0eaf77fd7bbab6400e65b16a1a33
ecuber <37519236+ecuber@users.noreply.github.com>
2020-05-09 14:09:17 -0400

    added 180 deg rotations

--- util/orient.js
+++ util/orient.js
@@ -4,7 +4,7 @@ module.exports = {
         let moves = puzzle === "222" || puzzle === "444" ? ["x", "y", "z"] : puzzle === "333" ? ["Rw", "Uw", "Fw"] : ["3Rw", "3Uw", "3Fw"];
         for (let i = 0; i < Math.floor(Math.random() * 3); i++) {
             let index = Math.floor(Math.random() * moves.length);
-            rotations += `${moves[index]}${Math.random() > 0.5 ? "" : "\'"} `;
+            rotations += `${moves[index]}${Math.random() > 2 / 3 ? "" : Math.random() > 0.5 ? "\'" : "2"} `;
             moves.splice(index, 1);
         }
         return rotations;
