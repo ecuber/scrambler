4f56e0608ccd984a3fa55e1952e91b270ef9a60c
ecuber <37519236+ecuber@users.noreply.github.com>
2020-05-14 14:37:43 -0400

    added guild event logging

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,9 +1,20 @@
 const { Event } = require("klasa");
+const { guildLog } = require("../settings.json");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = class extends Event {
     run(guild) {
         if (this.client.ready && guild.available && !this.client.options.preserveSettings) guild.settings.destroy().catch(() => null);
         console.log(`Left guild ${guild.name} [${guild.id}]`);
+        this.client.channels.cache.get(guildLog).send(
+            new MessageEmbed()
+                .setColor("#d1646d")
+                .setThumbnail(guild.iconURL)
+                .setTitle("Left Guild:")
+                .addField("Name", `**${guild.name}** [ID: ${guild.id}]`)
+                .addField("Owner", `**${guild.owner.user.username}#${guild.owner.user.discriminator}** [ID: ${guild.owner.id}]`)
+                .addField("Member Count", `${guild.members.size}`)
+                .setTimestamp());
         this.client.user.setActivity(`s!updates | Scrambling cubes for ${this.client.guilds.cache.size} servers!`);
     }
 };
