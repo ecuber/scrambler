3e66f77c00c596488112f15b7532de490816ef1e
Christopher Gwynn <31748254+Bacon1661@users.noreply.github.com>
2021-09-23 14:09:34 -0400

    Add slash commands for every scramble

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,8 @@
 // import { MongoDBProvider } from 'commando-provider-mongo'
 import * as settings from '../settings'
 import { REST } from '@discordjs/rest'
-import {
-  Routes,
-  APIApplicationCommandOption
-} from 'discord-api-types/v9'
-import {
-  Interaction,
-  Client,
-  Intents,
-  Collection
-} from 'discord.js'
+import { Routes, APIApplicationCommandOption } from 'discord-api-types/v9'
+import { Interaction, Client, Intents, Collection } from 'discord.js'
 import fs from 'fs'
 import path from 'path'
 
@@ -28,19 +20,36 @@ interface CommandData {
 interface Command {
   data: CommandData
   run: (interaction: Interaction) => Promise<void>
-  help: any
 }
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
-const commands = new Collection<string, Command>()
-
-const commandFiles = fs.readdirSync(path.join(__dirname, 'commands')).filter(file => file.endsWith('.ts'))
-
 const clientId = process.env.NODE_ENV === 'production' ? settings.prodId : settings.devId
+
+const commands = new Collection<string, Command>()
+const commandFiles = fs.readdirSync(path.join(__dirname, 'commands')).filter(file => file.endsWith('.ts'))
 
 for (const file of commandFiles) {
   const command: Command = require(`./commands/${file}`)
   commands.set(command.data.name, command)
+}
+
+const scrambleFunc = require('./commands/scrambles/scramble.ts')
+
+//  Create NNN scrambles
+for (let i = 1, max: number, bld: string, fmc: string; i < 8; i++, bld = '', fmc = '') {
+  max = i <= 4 ? 12 : 1.5 * Math.pow(i, 2) - 19.5 * i + 68
+  bld = i <= 5 ? 'bld' : null
+  fmc = i === 3 ? 'fmc' : null
+
+  const scramble: Command = scrambleFunc.default(`${i}x${i}`, max, bld, fmc)
+  commands.set(scramble.data.name, scramble)
+}
+
+//  Everything else listed in scrambles.json
+const scrambleList = require('./util/scrambles.json')
+for (const name in scrambleList) {
+  const scramble: Command = scrambleFunc.default(name, scrambleList[name].max, '', '', scrambleList[name].fullName)
+  commands.set(scramble.data.name, scramble)
 }
 
 const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
@@ -48,14 +57,12 @@ const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 (async () => {
   try {
     console.log('Started refreshing application (/) commands.')
-
     await rest.put(
       process.env.NODE_ENV === 'development'
         ? Routes.applicationGuildCommands(clientId, settings.guildId) // register commands as guild commands in development (instantly updates)
         : Routes.applicationCommands(clientId), // otherwise, register commands globally (updates slower, up to an hour)
       { body: commands.map(command => command.data) }
     )
-
     console.log('Successfully reloaded application (/) commands.')
   } catch (error) {
     console.error(error)
