fb556ee306f5f44a4b2155681691e2baeb632cca
Elijah Sippel <37519236+ecuber@users.noreply.github.com>
2023-01-04 17:56:49 -0500

    Add error catching for shards

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,17 @@
 import { ShardingManager } from 'discord.js'
 require('dotenv').config()
 
-const manager = new ShardingManager('./src/app.ts', { token: process.env.TOKEN, execArgv: ['-r', 'ts-node/register'] })
+const manager = new ShardingManager('./src/app.ts', {
+  token: process.env.TOKEN,
+  execArgv: ['-r', 'ts-node/register'],
+  respawn: true
+})
 
-manager.on('shardCreate', shard => { console.log(`Launched shard ${shard.id}`) })
+manager.on('shardCreate', async (shard) => {
+  console.log(`Launched shard ${shard.id}`)
+  shard.on('error', (error) => {
+    console.error(error)
+  })
+})
 
-manager.spawn({ amount: 'auto', delay: 15500, timeout: 60000 })
+manager.spawn({ amount: 'auto', delay: 15500, timeout: 60000 }).catch(e => console.log)
